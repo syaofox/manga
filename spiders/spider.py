3506ebe659a27efcf4207ddb5842755a6cba66f6
syaofox <syaofox@gmail.com>
2022-05-01 19:08:40 +0800

    页面关闭策略

--- spiders/spider.py
+++ spiders/spider.py
@@ -74,7 +74,12 @@ class Spider:
 
     async def fetch_page(self, url, browser: BrowserContext, parse_method, retry=0, param=None):
 
-        page = await browser.new_page()
+        pages = len(browser.pages)
+
+        if pages > 0:
+            page = browser.pages[0]
+        else:
+            page = await browser.new_page()
 
         try:
             await parse_method(browser, page, url, param)
@@ -91,5 +96,5 @@ class Spider:
                 Logouter.red(e.message)
                 Logouter.red(f'页面{url}打开错误,重试超过最大次数')
 
-        finally:
-            await page.close()
+        # finally:
+        #     await page.close()
